ac1f841133f2e7ccfa8092c70e10431ad088c300
Maxim Laskovets <Laskovets.max@gmail.com>
2022-03-13 22:34:41 +0300

    change: change border-radius in userItem, commentItem and postItem

--- src/components/post/Post.styles.tsx
+++ src/components/post/Post.styles.tsx
@@ -4,6 +4,7 @@ export const PostConteiner = styled.div`
   max-width: 100%;
   height: 100%;
   border: 1px solid yellow;
+  border-radius: 10px;
   background-color: grey;
   margin: 20px;
   cursor: pointer;
